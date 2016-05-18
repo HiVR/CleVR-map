860a1b4ee8bb07cf08bbb09ace31c048e3469f94
Adriaan de Vos <adriaan.devos+git@gmail.com>
2016-05-18 13:45:50 +0200

    Add documentation and fix StyleCop warnings

--- Assets/Networking/NetworkManager.cs
+++ Assets/Networking/NetworkManager.cs
@@ -1,69 +1,140 @@
-﻿using System.Collections.Generic;
+﻿// <copyright file="NetworkManager.cs" company="HiVR">
+// Copyright (c) 2016 HiVR All Rights Reserved
+// </copyright>
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using UnityEngine;
 
+/// <summary>
+/// This class is responsible for communicating with our client-map
+/// </summary>
 public class NetworkManager : MonoBehaviour
 {
     #region Fields
 
-    private const int bytesize = 1024 * 1024;
-    private TcpListener listener;
-    private List<TcpClient> sender;
-    private string message;
+    /// <summary>
+    /// Contains the byte size used in the buffer for receiving messages
+    /// </summary>
+    private const int BYTESIZE = 1024 * 1024;
+
+    /// <summary>
+    /// Contains the active server object
+    /// </summary>
+    private TcpListener server;
+
+    /// <summary>
+    /// Contains every client that has been connected
+    /// </summary>
+    private List<TcpClient> clientList;
+
+    /// <summary>
+    /// Counts the frames because some things don't need to happen every frame
+    /// </summary>
+    private int frameCounter = 0;
 
     #endregion Fields
 
     #region Methods
 
-    private static string cleanMessage(byte[] bytes)
+    /// <summary>
+    /// This method is responsible for receiving a buffer and converting it to a ASCII message
+    /// </summary>
+    /// <param name="bytes">Buffer with the client message</param>
+    /// <returns>String that contains ASCII message</returns>
+    private static string CleanMessage(byte[] bytes)
     {
         string message = System.Text.Encoding.ASCII.GetString(bytes);
 
-        string messageToPrint = null;
-        foreach (var nullChar in message)
+        string res = string.Empty;
+        foreach (char nullChar in message)
         {
             if (nullChar != '\0')
             {
-                messageToPrint += nullChar;
+                res += nullChar;
             }
         }
-        return messageToPrint;
+
+        return res;
     }
 
-    // Use this for initialization
+    /// <summary>
+    /// Initialize the NetworkManger on Unity startup by starting the server
+    /// </summary>
     private void Start()
     {
         Application.runInBackground = true; // Unity will continue running in the background
 
-        sender = new List<TcpClient>();
+        this.clientList = new List<TcpClient>();
 
-        IPEndPoint endpoint = new IPEndPoint(IPAddress.Loopback, 8080); // Address + port
-        listener = new TcpListener(endpoint); // Instantiate the object
-        listener.Start(); // Start listening...
+        IPEndPoint endpoint = new IPEndPoint(IPAddress.Loopback, 25565); // Address + port
+        this.server = new TcpListener(endpoint); // Instantiate the object
+        this.server.Start(); // Start listening...
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// This method will be called every frame and contains calls to various methods
+    /// </summary>
     private void Update()
     {
-        if (listener.Pending())
-        {
-            TcpClient newClient = listener.AcceptTcpClient();
-            sender.Add(newClient);
-            Debug.Log("Client connected from " + newClient.Client.LocalEndPoint.ToString());
-        }
+        this.frameCounter++;
+        this.AcceptNewConnection();
 
-        foreach (TcpClient client in sender)
+        foreach (TcpClient client in this.clientList)
         {
-            if (client.GetStream().DataAvailable)
-            {
-                byte[] buffer = new byte[bytesize];
-                client.GetStream().Read(buffer, 0, bytesize);
-                message = cleanMessage(buffer);
-                Debug.Log(message);
-            }
+            this.ReadMessage(client);
+            this.SentDynamic(client);
 
             // TODO Implement pings to check if connection is alive
+        }
+    }
+
+    /// <summary>
+    /// This method checks if there is a client trying to make a connection and accepts it
+    /// </summary>
+    private void AcceptNewConnection()
+    {
+        if (this.server.Pending())
+        {
+            TcpClient newClient = this.server.AcceptTcpClient();
+            this.clientList.Add(newClient);
+            Debug.Log("Client connected from " + newClient.Client.LocalEndPoint.ToString());
+            this.SentStatic(newClient);
+        }
+    }
+
+    /// <summary>
+    /// This method checks if there is a message from the client and reads it
+    /// </summary>
+    /// <param name="client">The client that will get checked</param>
+    private void ReadMessage(TcpClient client)
+    {
+        if (client.GetStream().DataAvailable)
+        {
+            byte[] buffer = new byte[BYTESIZE];
+            client.GetStream().Read(buffer, 0, BYTESIZE);
+            string message = CleanMessage(buffer);
+            Debug.Log(message);
+        }
+    }
+
+    /// <summary>
+    /// This method will sent all the static objects to a client
+    /// </summary>
+    /// <param name="client">The client that will receive all static objects</param>
+    private void SentStatic(TcpClient client)
+    {
+    }
+
+    /// <summary>
+    /// This method will sent the dynamic objects to a client
+    /// </summary>
+    /// <param name="client">The client that will receive the dynamic objects</param>
+    private void SentDynamic(TcpClient client)
+    {
+        if (this.frameCounter % 30 == 0)
+        {
+            this.frameCounter = 0;
         }
     }
 
