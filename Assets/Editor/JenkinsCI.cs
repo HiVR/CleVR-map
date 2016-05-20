39df998c4a503a55b2269930d83f059a8ff27057
Boris Schrijver <bschrijver@schubergphilis.com>
2016-05-20 18:32:32 +0200

    Add commentary to Assets/Editor/JenkinsCI.cs

--- Assets/Editor/JenkinsCI.cs
+++ Assets/Editor/JenkinsCI.cs
@@ -1,39 +1,75 @@
-﻿using UnityEditor;
-using System.Collections.Generic;
-using System;
-
-public class JenkinsCI
+﻿// <copyright file="JenkinsCI.cs" company="HiVR">
+//     HiVR All rights reserved.
+// </copyright>
+namespace Assets.Editor
 {
-    static string[] SCENES = FindEnabledEditorScenes();
+    using UnityEditor;
+    using System.Collections.Generic;
+    using System;
 
-    static string APP_NAME = "CleVR-map";
-    static string TARGET_DIR = "target";
-
-    [MenuItem("Custom/CI/Build on Jenkins CI")]
-    static void PerformWindowsBuild()
+    /// <summary>
+    /// JenkinsCI is used as en entry point for testing.
+    /// </summary>
+    public class JenkinsCI
     {
-        string target_dir = APP_NAME + ".exe";
-        GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneWindows, BuildOptions.None);
-    }
+        #region Fields
 
-    private static string[] FindEnabledEditorScenes()
-    {
-        List<string> EditorScenes = new List<string>();
-        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        /// <summary>
+        /// An array of all scenes.
+        /// </summary>
+        static string[] SCENES = FindEnabledEditorScenes();
+
+        /// <summary>
+        /// The name of the app.
+        /// </summary>
+        static string APP_NAME = "CleVR-map";
+
+        /// <summary>
+        /// Target directory.
+        /// </summary>
+        static string TARGET_DIR = "target";
+
+        #endregion Fields
+
+        #region Methods
+
+        /// <summary>
+        /// Entry point to start a build.
+        /// </summary>
+        [MenuItem("Custom/CI/Build on Jenkins CI")]
+        static void PerformWindowsBuild()
         {
-            if (!scene.enabled) continue;
-            EditorScenes.Add(scene.path);
+            string target_dir = APP_NAME + ".exe";
+            GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneWindows, BuildOptions.None);
         }
-        return EditorScenes.ToArray();
-    }
 
-    static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
-    {
-        EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
-        string res = BuildPipeline.BuildPlayer(scenes, target_dir, build_target, build_options);
-        if (res.Length > 0)
+        /// <summary>
+        /// Find all scenes.
+        /// </summary>
+        private static string[] FindEnabledEditorScenes()
         {
-            throw new Exception("BuildPlayer failure: " + res);
+            List<string> EditorScenes = new List<string>();
+            foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+            {
+                if (!scene.enabled) continue;
+                EditorScenes.Add(scene.path);
+            }
+            return EditorScenes.ToArray();
         }
+
+        /// <summary>
+        /// Start a build on all collected scenes, into the target folder.
+        /// </summary>
+        static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+        {
+            EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+            string res = BuildPipeline.BuildPlayer(scenes, target_dir, build_target, build_options);
+            if (res.Length > 0)
+            {
+                throw new Exception("BuildPlayer failure: " + res);
+            }
+        }
+
+        #endregion Methods
     }
 }
